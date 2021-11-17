1fd6d611dcd1bcaa2f41605b969a10749a80b7c7
tanjilulkarim <tanjilkarim580@gmail.com>
2021-11-17 17:45:06 +0600

    fourth commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 console.log(uri);
-
+////////
 async function run() {
   try {
     await client.connect();

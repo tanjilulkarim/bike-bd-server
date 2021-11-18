6ff42593c1eebf0b1ca9526d9f1e1afc884c162c
tanjilulkarim <tanjilkarim580@gmail.com>
2021-11-18 17:04:54 +0600

    fourth-update

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function run() {
       res.send(result);
     });
 
-    // get single product
+    // get single product//
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
